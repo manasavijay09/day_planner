18da1c397895b920167124f320d5308be10343e6
manasavijay09 <manasa.vijay09@gmail.com>
2023-12-01 20:51:40 -0600

    saving in progress

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  function saveHandler(){
+    let apptText=$(this).siblings(".description").val();
+    console.log("apptText = ", apptText);
+  }
+  $(".saveBtn").on("click", saveHandler);
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
